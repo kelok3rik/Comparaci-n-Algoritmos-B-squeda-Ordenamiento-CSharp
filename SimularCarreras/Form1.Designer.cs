5c651b598365da22b9bc21f47a3d89cd4e2e3edf
kelok3rik <erikrdbs@gmail.com>
2024-02-04 18:13:11 -0400

    FINAL

--- SimularCarreras/Form1.Designer.cs
+++ SimularCarreras/Form1.Designer.cs
@@ -28,13 +28,334 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.arregloTxt = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.t_memoria = new System.Windows.Forms.Label();
+            this.labelInsercion = new System.Windows.Forms.Label();
+            this.labelQuik = new System.Windows.Forms.Label();
+            this.labelBurbuja = new System.Windows.Forms.Label();
+            this.labelBinaria = new System.Windows.Forms.Label();
+            this.labelSecuencial = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelTiempoSecuencial = new System.Windows.Forms.Label();
+            this.labelTiempoBinario = new System.Windows.Forms.Label();
+            this.labelTiempoQuickSort = new System.Windows.Forms.Label();
+            this.labelTiempoBurbuja = new System.Windows.Forms.Label();
+            this.labelMetodoInsercion = new System.Windows.Forms.Label();
+            this.labelMemoriaConsumida = new System.Windows.Forms.Label();
+            this.labelResultadoCarrera = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBoxBuscar = new System.Windows.Forms.TextBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelResultadoCarreraOrd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // arregloTxt
+            // 
+            this.arregloTxt.Location = new System.Drawing.Point(12, 132);
+            this.arregloTxt.Name = "arregloTxt";
+            this.arregloTxt.Size = new System.Drawing.Size(545, 223);
+            this.arregloTxt.TabIndex = 0;
+            this.arregloTxt.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Britannic Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(220, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(301, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CARRERA ALGORITMO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Arreglo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 375);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "TIEMPOS";
+            // 
+            // t_memoria
+            // 
+            this.t_memoria.AutoSize = true;
+            this.t_memoria.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.t_memoria.Location = new System.Drawing.Point(26, 575);
+            this.t_memoria.Name = "t_memoria";
+            this.t_memoria.Size = new System.Drawing.Size(253, 23);
+            this.t_memoria.TabIndex = 30;
+            this.t_memoria.Text = "Total de memoria consumida:";
+            // 
+            // labelInsercion
+            // 
+            this.labelInsercion.AutoSize = true;
+            this.labelInsercion.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInsercion.Location = new System.Drawing.Point(26, 542);
+            this.labelInsercion.Name = "labelInsercion";
+            this.labelInsercion.Size = new System.Drawing.Size(181, 23);
+            this.labelInsercion.TabIndex = 29;
+            this.labelInsercion.Text = "Metodo de Insercion";
+            // 
+            // labelQuik
+            // 
+            this.labelQuik.AutoSize = true;
+            this.labelQuik.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelQuik.Location = new System.Drawing.Point(26, 510);
+            this.labelQuik.Name = "labelQuik";
+            this.labelQuik.Size = new System.Drawing.Size(81, 23);
+            this.labelQuik.TabIndex = 28;
+            this.labelQuik.Text = "QuikSort";
+            // 
+            // labelBurbuja
+            // 
+            this.labelBurbuja.AutoSize = true;
+            this.labelBurbuja.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBurbuja.Location = new System.Drawing.Point(26, 476);
+            this.labelBurbuja.Name = "labelBurbuja";
+            this.labelBurbuja.Size = new System.Drawing.Size(169, 23);
+            this.labelBurbuja.TabIndex = 27;
+            this.labelBurbuja.Text = "Metodo de Burbuja";
+            // 
+            // labelBinaria
+            // 
+            this.labelBinaria.AutoSize = true;
+            this.labelBinaria.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBinaria.Location = new System.Drawing.Point(26, 442);
+            this.labelBinaria.Name = "labelBinaria";
+            this.labelBinaria.Size = new System.Drawing.Size(150, 23);
+            this.labelBinaria.TabIndex = 26;
+            this.labelBinaria.Text = "Busqueda Binaria";
+            // 
+            // labelSecuencial
+            // 
+            this.labelSecuencial.AutoSize = true;
+            this.labelSecuencial.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSecuencial.Location = new System.Drawing.Point(26, 411);
+            this.labelSecuencial.Name = "labelSecuencial";
+            this.labelSecuencial.Size = new System.Drawing.Size(182, 23);
+            this.labelSecuencial.TabIndex = 25;
+            this.labelSecuencial.Text = "Busqueda Secuencial";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(563, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 38);
+            this.button1.TabIndex = 31;
+            this.button1.Text = "EJECUTAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 611);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 25);
+            this.label4.TabIndex = 32;
+            this.label4.Text = "RESULTADO:";
+            // 
+            // labelTiempoSecuencial
+            // 
+            this.labelTiempoSecuencial.AutoSize = true;
+            this.labelTiempoSecuencial.Location = new System.Drawing.Point(223, 416);
+            this.labelTiempoSecuencial.Name = "labelTiempoSecuencial";
+            this.labelTiempoSecuencial.Size = new System.Drawing.Size(44, 16);
+            this.labelTiempoSecuencial.TabIndex = 33;
+            this.labelTiempoSecuencial.Text = "label5";
+            // 
+            // labelTiempoBinario
+            // 
+            this.labelTiempoBinario.AutoSize = true;
+            this.labelTiempoBinario.Location = new System.Drawing.Point(223, 447);
+            this.labelTiempoBinario.Name = "labelTiempoBinario";
+            this.labelTiempoBinario.Size = new System.Drawing.Size(44, 16);
+            this.labelTiempoBinario.TabIndex = 34;
+            this.labelTiempoBinario.Text = "label5";
+            // 
+            // labelTiempoQuickSort
+            // 
+            this.labelTiempoQuickSort.AutoSize = true;
+            this.labelTiempoQuickSort.Location = new System.Drawing.Point(223, 517);
+            this.labelTiempoQuickSort.Name = "labelTiempoQuickSort";
+            this.labelTiempoQuickSort.Size = new System.Drawing.Size(44, 16);
+            this.labelTiempoQuickSort.TabIndex = 36;
+            this.labelTiempoQuickSort.Text = "label5";
+            // 
+            // labelTiempoBurbuja
+            // 
+            this.labelTiempoBurbuja.AutoSize = true;
+            this.labelTiempoBurbuja.Location = new System.Drawing.Point(223, 483);
+            this.labelTiempoBurbuja.Name = "labelTiempoBurbuja";
+            this.labelTiempoBurbuja.Size = new System.Drawing.Size(44, 16);
+            this.labelTiempoBurbuja.TabIndex = 35;
+            this.labelTiempoBurbuja.Text = "label5";
+            // 
+            // labelMetodoInsercion
+            // 
+            this.labelMetodoInsercion.AutoSize = true;
+            this.labelMetodoInsercion.Location = new System.Drawing.Point(223, 547);
+            this.labelMetodoInsercion.Name = "labelMetodoInsercion";
+            this.labelMetodoInsercion.Size = new System.Drawing.Size(44, 16);
+            this.labelMetodoInsercion.TabIndex = 37;
+            this.labelMetodoInsercion.Text = "label7";
+            // 
+            // labelMemoriaConsumida
+            // 
+            this.labelMemoriaConsumida.AutoSize = true;
+            this.labelMemoriaConsumida.Location = new System.Drawing.Point(305, 580);
+            this.labelMemoriaConsumida.Name = "labelMemoriaConsumida";
+            this.labelMemoriaConsumida.Size = new System.Drawing.Size(44, 16);
+            this.labelMemoriaConsumida.TabIndex = 38;
+            this.labelMemoriaConsumida.Text = "label7";
+            // 
+            // labelResultadoCarrera
+            // 
+            this.labelResultadoCarrera.AutoSize = true;
+            this.labelResultadoCarrera.Location = new System.Drawing.Point(198, 652);
+            this.labelResultadoCarrera.Name = "labelResultadoCarrera";
+            this.labelResultadoCarrera.Size = new System.Drawing.Size(44, 16);
+            this.labelResultadoCarrera.TabIndex = 39;
+            this.labelResultadoCarrera.Text = "label7";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(558, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 25);
+            this.label5.TabIndex = 40;
+            this.label5.Text = "Buscar";
+            // 
+            // txtBoxBuscar
+            // 
+            this.txtBoxBuscar.Location = new System.Drawing.Point(637, 132);
+            this.txtBoxBuscar.Name = "txtBoxBuscar";
+            this.txtBoxBuscar.Size = new System.Drawing.Size(103, 22);
+            this.txtBoxBuscar.TabIndex = 41;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerar.Location = new System.Drawing.Point(563, 179);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(143, 38);
+            this.btnGenerar.TabIndex = 42;
+            this.btnGenerar.Text = "GENERAR";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 652);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(150, 16);
+            this.label6.TabIndex = 43;
+            this.label6.Text = "Algoritmo de busqueda:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(30, 681);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(172, 16);
+            this.label7.TabIndex = 44;
+            this.label7.Text = "Algoritmo de ordenamiento:";
+            // 
+            // labelResultadoCarreraOrd
+            // 
+            this.labelResultadoCarreraOrd.AutoSize = true;
+            this.labelResultadoCarreraOrd.Location = new System.Drawing.Point(208, 681);
+            this.labelResultadoCarreraOrd.Name = "labelResultadoCarreraOrd";
+            this.labelResultadoCarreraOrd.Size = new System.Drawing.Size(44, 16);
+            this.labelResultadoCarreraOrd.TabIndex = 45;
+            this.labelResultadoCarreraOrd.Text = "label7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(752, 706);
+            this.Controls.Add(this.labelResultadoCarreraOrd);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.txtBoxBuscar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelResultadoCarrera);
+            this.Controls.Add(this.labelMemoriaConsumida);
+            this.Controls.Add(this.labelMetodoInsercion);
+            this.Controls.Add(this.labelTiempoQuickSort);
+            this.Controls.Add(this.labelTiempoBurbuja);
+            this.Controls.Add(this.labelTiempoBinario);
+            this.Controls.Add(this.labelTiempoSecuencial);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.t_memoria);
+            this.Controls.Add(this.labelInsercion);
+            this.Controls.Add(this.labelQuik);
+            this.Controls.Add(this.labelBurbuja);
+            this.Controls.Add(this.labelBinaria);
+            this.Controls.Add(this.labelSecuencial);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.arregloTxt);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox arregloTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label t_memoria;
+        private System.Windows.Forms.Label labelInsercion;
+        private System.Windows.Forms.Label labelQuik;
+        private System.Windows.Forms.Label labelBurbuja;
+        private System.Windows.Forms.Label labelBinaria;
+        private System.Windows.Forms.Label labelSecuencial;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelTiempoSecuencial;
+        private System.Windows.Forms.Label labelTiempoBinario;
+        private System.Windows.Forms.Label labelTiempoQuickSort;
+        private System.Windows.Forms.Label labelTiempoBurbuja;
+        private System.Windows.Forms.Label labelMetodoInsercion;
+        private System.Windows.Forms.Label labelMemoriaConsumida;
+        private System.Windows.Forms.Label labelResultadoCarrera;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBoxBuscar;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelResultadoCarreraOrd;
     }
 }
 
--- SimularCarreras/Form1.cs
+++ SimularCarreras/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,280 @@ namespace SimularCarreras
 {
     public partial class Form1 : Form
     {
+
+        // Generar un nuevo arreglo aleatorio
+        int[] arreglo;
+
+
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            if (txtBoxBuscar.Text == "" || arregloTxt.Text == "")
+            {
+                MessageBox.Show("INGRESE UN VALOR"); return;
+            }
+
+            int valorBuscar = Convert.ToInt32(txtBoxBuscar.Text);
+
+            arreglo = arregloTxt.Text.Split(',').Select(int.Parse).ToArray();
+
+
+            Console.WriteLine("Arreglo con el que se trabaja: " + string.Join(", ", arreglo));
+
+            // Crear tareas para ejecutar cada algoritmo simultáneamente
+
+            Task tareaInsercion = Task.Run(() =>
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+                Insercion(arreglo);
+                TiempoInsercion = sw.Elapsed;
+            });
+
+            Task<int> tareaBusquedaSecuencial = Task.Run(() =>
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+                int resultado = BusquedaSecuencial(arreglo, valorBuscar);
+                TiempoBusquedaSecuencial = sw.Elapsed;
+                return resultado;
+            });
+
+            Task<int> tareaBusquedaBinaria = Task.Run(() =>
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+                int resultado = BusquedaBinaria(arreglo, valorBuscar);
+                TiempoBusquedaBinaria = sw.Elapsed;
+                return resultado;
+            });
+
+            Task tareaBurbuja = Task.Run(() =>
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+                Burbuja(arreglo);
+                TiempoBurbuja = sw.Elapsed;
+            });
+
+            Task tareaQuickSort = Task.Run(() =>
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+                QuickSort(arreglo, 0, arreglo.Length - 1);
+                TiempoQuickSort = sw.Elapsed;
+            });
+
+
+
+            // Obtener la memoria consumida por el proceso
+            long memoriaConsumida = Process.GetCurrentProcess().PrivateMemorySize64;
+            labelMemoriaConsumida.Text = $"Memoria consumida: {memoriaConsumida / 1024 / 1024} MB";
+
+
+
+
+            // Inicializa las variables para guardar los tiempos más rápidos
+            string algoritmoBusquedaMasRapido = "";
+            TimeSpan tiempoBusquedaMasRapido = TimeSpan.MaxValue;
+            string algoritmoOrdenamientoMasRapido = "";
+            TimeSpan tiempoOrdenamientoMasRapido = TimeSpan.MaxValue;
+
+            // Después de que todas las tareas estén completas, mostrar resultados
+            Task.WhenAll(tareaBusquedaSecuencial, tareaBusquedaBinaria, tareaBurbuja, tareaQuickSort, tareaInsercion)
+                .ContinueWith((task) =>
+                {
+
+
+                    // Mostrar los tiempos de ejecución de cada algoritmo
+                    this.Invoke((Action)(() =>
+                    {
+                        labelTiempoSecuencial.Text = $"Tiempo de Búsqueda Secuencial: {TiempoBusquedaSecuencial}";
+                        labelTiempoBinario.Text = $"Tiempo de Búsqueda Binaria: {TiempoBusquedaBinaria}";
+                        labelTiempoBurbuja.Text = $"Tiempo de Ordenamiento Burbuja: {TiempoBurbuja}";
+                        labelTiempoQuickSort.Text = $"Tiempo de Quick Sort: {TiempoQuickSort}";
+                        labelMetodoInsercion.Text = $"Tiempo de Método de Inserción: {TiempoInsercion}";
+                    }));
+
+
+
+
+                    // Compara y actualiza los tiempos más rápidos para la búsqueda
+                    if (TiempoBusquedaSecuencial < tiempoBusquedaMasRapido)
+                    {
+                        algoritmoBusquedaMasRapido = "Búsqueda Secuencial";
+                        tiempoBusquedaMasRapido = TiempoBusquedaSecuencial;
+                    }
+
+                    if (TiempoBusquedaBinaria < tiempoBusquedaMasRapido)
+                    {
+                        algoritmoBusquedaMasRapido = "Búsqueda Binaria";
+                        tiempoBusquedaMasRapido = TiempoBusquedaBinaria;
+                    }
+
+                    // Compara y actualiza los tiempos más rápidos para el ordenamiento
+                    if (TiempoBurbuja < tiempoOrdenamientoMasRapido)
+                    {
+                        algoritmoOrdenamientoMasRapido = "Ordenamiento Burbuja";
+                        tiempoOrdenamientoMasRapido = TiempoBurbuja;
+                    }
+
+                    if (TiempoQuickSort < tiempoOrdenamientoMasRapido)
+                    {
+                        algoritmoOrdenamientoMasRapido = "Quick Sort";
+                        tiempoOrdenamientoMasRapido = TiempoQuickSort;
+                    }
+
+                    if (TiempoInsercion < tiempoOrdenamientoMasRapido)
+                    {
+                        algoritmoOrdenamientoMasRapido = "Método de Inserción";
+                        tiempoOrdenamientoMasRapido = TiempoInsercion;
+                    }
+
+                    this.Invoke((Action)(() =>
+                    {
+                        labelResultadoCarrera.Text = $"El ganador de la carrera fue {algoritmoBusquedaMasRapido} con un tiempo de ejecución de {tiempoBusquedaMasRapido}";
+                        labelResultadoCarreraOrd.Text = $"El ganador de la carrera fue {algoritmoOrdenamientoMasRapido} con un tiempo de ejecución de {tiempoOrdenamientoMasRapido}";
+                    }));
+
+                });
+
+
+
+
+        }
+
+        // Variables para almacenar los tiempos de ejecución de los algoritmos
+        TimeSpan TiempoBusquedaSecuencial;
+        TimeSpan TiempoBusquedaBinaria;
+        TimeSpan TiempoBurbuja;
+        TimeSpan TiempoQuickSort;
+        TimeSpan TiempoInsercion;
+
+        static int[] GenerarArregloAleatorio(int tamano)
+        {
+            Random rnd = new Random();
+            int[] arreglo = new int[tamano];
+            for (int i = 0; i < tamano; i++)
+            {
+                arreglo[i] = rnd.Next(0, 10000); // Números aleatorios del 0 al 999
+            }
+            return arreglo;
+
+        }
+
+        // Implementación de los algoritmos de búsqueda y ordenamiento
+
+        static int BusquedaSecuencial(int[] arreglo, int valor)
+        {
+            Console.WriteLine("Busqueda secuencial a buscar: " + valor);
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                if (arreglo[i] == valor)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        static int BusquedaBinaria(int[] arreglo, int valor)
+        {
+           Console.WriteLine("Busqueda binaria a buscar: "+valor);
+            int inicio = 0;
+            int fin = arreglo.Length - 1;
+
+            while (inicio <= fin)
+            {
+                int medio = (inicio + fin) / 2;
+                if (arreglo[medio] == valor)
+                {
+                    return medio;
+                }
+                else if (arreglo[medio] < valor)
+                    inicio = medio + 1;
+                else
+                    fin = medio - 1;
+            }
+
+            return -1;
+        }
+
+        static void Burbuja(int[] arreglo)
+        {
+            for (int i = 0; i < arreglo.Length - 1; i++)
+            {
+                for (int j = 0; j < arreglo.Length - 1 - i; j++)
+                {
+                    if (arreglo[j] > arreglo[j + 1])
+                    {
+                        int temp = arreglo[j];
+                        arreglo[j] = arreglo[j + 1];
+                        arreglo[j + 1] = temp;
+                    }
+                }
+            }
+
+        }
+
+        static void QuickSort(int[] arreglo, int izquierda, int derecha)
+        {
+            if (izquierda < derecha)
+            {
+                int indiceParticion = Particionar(arreglo, izquierda, derecha);
+
+                QuickSort(arreglo, izquierda, indiceParticion - 1);
+                QuickSort(arreglo, indiceParticion + 1, derecha);
+            }
+        }
+
+        static int Particionar(int[] arreglo, int izquierda, int derecha)
+        {
+            int pivote = arreglo[derecha];
+            int indiceMenor = izquierda - 1;
+
+            for (int j = izquierda; j < derecha; j++)
+            {
+                if (arreglo[j] <= pivote)
+                {
+                    indiceMenor++;
+                    int temp = arreglo[indiceMenor];
+                    arreglo[indiceMenor] = arreglo[j];
+                    arreglo[j] = temp;
+                }
+            }
+
+            int temp1 = arreglo[indiceMenor + 1];
+            arreglo[indiceMenor + 1] = arreglo[derecha];
+            arreglo[derecha] = temp1;
+
+            return indiceMenor + 1;
+        }
+
+        static void Insercion(int[] arreglo)
+        {
+            for (int i = 1; i < arreglo.Length; i++)
+            {
+                int clave = arreglo[i];
+                int j = i - 1;
+
+                while (j >= 0 && arreglo[j] > clave)
+                {
+                    arreglo[j + 1] = arreglo[j];
+                    j = j - 1;
+                }
+                arreglo[j + 1] = clave;
+            }
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            arreglo = GenerarArregloAleatorio(10000);
+
+            // Mostrar el arreglo en el TextBox
+            arregloTxt.Text = string.Join(", ", arreglo);
         }
     }
 }
